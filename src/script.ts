3a73a30b8d65b35d6566d5cca7a6b1b640a22b00
ToastHawaii <mar.pel@protonmail.com>
2020-05-23 16:35:50 +0200

    add support for touch title

--- src/script.ts
+++ src/script.ts
@@ -24,3 +24,21 @@ else
   );
 
 import "details-element-polyfill";
+
+document.addEventListener("click", function (e) {
+  const target = (e.target as HTMLElement).parentElement;
+
+  const titleElement = document.querySelector(".attribut .title");
+  if (titleElement) titleElement.remove();
+
+  if (target && target.classList.contains("attribut")) {
+    const titleElement = document.createElement("span");
+    titleElement.className = "title";
+    titleElement.innerHTML = target.title;
+    target.append(titleElement);
+
+    setTimeout(() => {
+      titleElement.remove();
+    }, 2000);
+  }
+});
