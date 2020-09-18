aab3995f29e016caca76189241047724a39eaa7a
ToastHawaii <mar.pel@protonmail.com>
2020-09-18 19:07:24 +0200

    Add license

--- src/filters.ts
+++ src/filters.ts
@@ -1,3 +1,20 @@
+// Copyright (C) 2020 Markus Peloso
+// 
+// This file is part of Priceless map.
+// 
+// Priceless map is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// Priceless map is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with Priceless map.  If not, see <http://www.gnu.org/licenses/>.
+
 function nwFee(tag: string) {
   return `nw${tag}["fee"];
 nw${tag}["fee:conditional"];`;
@@ -612,13 +629,13 @@ nw["landuse"="community_food_growing"];`,
     value: "drinking-water",
     icon: "https://wiki.openstreetmap.org/w/images/0/08/Drinking-water-16.svg",
     query: `
-nwr["drinking_water"="yes"];
+nwr["drinking_water"="yes"]["drinking_water:legal"!="no"];
 
-nwr["amenity"="drinking_water"];
+nwr["amenity"="drinking_water"]["drinking_water:legal"!="no"];
 
-node["amenity"="water_point"];
+node["amenity"="water_point"]["drinking_water:legal"!="no"];
 
-nw["drinking_water:refill"="yes"];`,
+nw["drinking_water:refill"="yes"]["drinking_water:legal"!="no"];`,
     color: "#4169E1",
     tags: [
       "amenity=drinking_water",
--- src/utilities/image.ts
+++ src/utilities/image.ts
@@ -1,3 +1,20 @@
+// Copyright (C) 2020 Markus Peloso
+// 
+// This file is part of Priceless map.
+// 
+// Priceless map is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// Priceless map is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with Priceless map.  If not, see <http://www.gnu.org/licenses/>.
+
 import md5 = require("md5");
 import { httpRegex } from "./url";
 import { startsWithIgnoreCase } from "./string";
