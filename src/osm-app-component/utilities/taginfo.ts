1ce660a054c66127f621a03c0b3e0d2dd084d4b2
ToastHawaii <mar.pel@protonmail.com>
2024-07-08 09:24:49 +0200

    Update taginfo add description

--- src/osm-app-component/utilities/taginfo.ts
+++ src/osm-app-component/utilities/taginfo.ts
@@ -1,12 +1,37 @@
+import { TFunction } from "i18next";
 import { Filter } from "../control/Filters";
 
-export function printTagInfoList(filters: Filter[]) {
+export function printTagInfoList(
+  t: TFunction<"translation", undefined>,
+  urlBase: string,
+  filters: Filter[]
+) {
   console.info(
     JSON.stringify(
-      [...new Set(filters.flatMap((f) => f.tags))].map((f) => {
-        const keyValue = f.split("=");
+      [...new Set(filters.flatMap((f) => f.tags))].map((tag) => {
+        const fs = filters.filter((o) => o.tags.find((t2) => t2 === tag));
+        const keyValue = tag.split("=");
         if (keyValue.length > 1 && keyValue[1] !== "*") {
-          return { key: keyValue[0], value: keyValue[1] };
+          return {
+            key: keyValue[0],
+            value: keyValue[1],
+            description: fs
+              .map(
+                (fs) =>
+                  t("group." + fs.group) + "/" + t("type." + fs.value + ".name")
+              )
+              .join(", "),
+            doc_url:
+              urlBase +
+              "?offers=" +
+              fs[0].group +
+              "/" +
+              fs[0].value +
+              "&info=" +
+              fs[0].group +
+              "/" +
+              fs[0].value,
+          };
         }
         return { key: keyValue[0] };
       })
